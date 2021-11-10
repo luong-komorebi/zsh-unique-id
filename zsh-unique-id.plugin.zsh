affcb3afc79cc75f558c658df5a7a52d2e49e320
Philipp Schmitt <philipp@schmitt.co>
2021-11-10 18:27:52 +0100

    Update url

--- zsh-unique-id.plugin.zsh
+++ zsh-unique-id.plugin.zsh
@@ -1,5 +1,5 @@
 # Set up $0 according to the standard:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 local ZERO="$0"
 
